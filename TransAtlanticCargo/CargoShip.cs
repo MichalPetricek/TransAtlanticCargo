75b54951fe51ad5df9f9aaafa4213b2f32829e4d
MichalPetricek <57211483+MichalPetricek@users.noreply.github.com>
2020-05-09 17:22:15 +0200

    Add files via upload

---
+++ TransAtlanticCargo/CargoShip.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransAtlanticCargo
+{
+    class CargoShip
+    {
+        public CargoShip(int containerPrice = 50, int loadPrice = 1, double loadWastePercent = 0.5)
+        {
+            ContainerPrice = containerPrice;
+            LoadPrice = loadPrice;
+            LoadWastePercent = loadWastePercent;
+        }
+        public int ContainerPrice { get; protected set; }
+        public int LoadPrice { get; protected set; }
+        public double LoadWastePercent { get; protected set; }
+        public List<int> _containerAmmount = new List<int>();
+        public string Output { get; protected set; }
+        public int Counter { get; protected set; }
+        public int RealAmmount { get; protected set; }
+        public double VolumePrice { get; protected set; }
+        public string Register(int item)
+        {
+            if (item <= 1000 && item >= 1)
+            {
+                _containerAmmount.Add(item);
+                return item + "t container registered...";
+            }
+            else
+            {
+                return item + "t container cannot be registered...";
+            }
+        }
+        
+        public int FixedPrice 
+        {
+            get
+            {
+                return ContainerPrice * RealAmmount;
+            }
+        }
+        public bool Load(int cargoWeight)
+        {
+            if (cargoWeight > 0)
+            {
+                _containerAmmount.Sort();
+                _containerAmmount.Reverse();
+                while (cargoWeight > 0)
+                {
+                    for (int i = 0; i < _containerAmmount.Count; i++)
+                    {
+                        Counter = 0;
+                        while (cargoWeight >= _containerAmmount[i])
+                        {
+                            cargoWeight = cargoWeight - _containerAmmount[i];
+                            VolumePrice += _containerAmmount[i] * LoadPrice;
+                            Counter++;
+                        }
+                        if (Counter > 0)
+                        {
+                            Output += Counter + "x " + _containerAmmount[i] + " t container \n";
+                        }
+                        RealAmmount += Counter;
+                    }
+                    break;
+                }
+                if (cargoWeight > 0)
+                {
+                    Output += "1x " + _containerAmmount[_containerAmmount.Count - 1] + " t container not fully loaded \n";
+                    VolumePrice += cargoWeight * LoadPrice;
+                    VolumePrice += (_containerAmmount[_containerAmmount.Count - 1] - cargoWeight) * LoadWastePercent;
+                    RealAmmount++;
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public double TotalPrice
+        {
+            get
+            {
+                return VolumePrice + FixedPrice;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Cargo roster: \n" + Output;
+        }
+    }
+}
---
+++ TransAtlanticCargo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static System.Console;
+
+namespace TransAtlanticCargo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OutputEncoding = System.Text.Encoding.Default;
+
+            CargoShip cargo1 = new CargoShip(50, 1, 0.5);
+
+
+            foreach (var weight in new int[] { 850, 400, 250, 50, 10 })
+                WriteLine(cargo1.Register(weight));
+
+            int cargoWeight = 3733; //načtení dat od např. uživatele
+
+            WriteLine($"\nTotal cargo weight: {cargoWeight} tonnes.");
+
+            Write("I am loading a container ship... ");
+
+            if (cargo1.Load(cargoWeight)) WriteLine("Loading complete!");
+            else WriteLine("Loading failed!");
+
+            WriteLine("Request to print the roster received...");
+            WriteLine(cargo1);
+
+            Write($"*Receipt* Fixed price: €{cargo1.FixedPrice}; ");
+            Write($"Volume price: €{cargo1.VolumePrice}; ");
+            WriteLine($"Total cost: €{cargo1.FixedPrice + cargo1.VolumePrice}");
+        }
+    }
+}
